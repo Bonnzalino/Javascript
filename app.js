1a2b37cf816d253ad610a4b5beb1752686048f9c
Bonnzalino <siwakorn.paorod@gmail.com>
2023-03-09 16:14:45 +0700

    Test Const

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-function myFunction() {
-    alert('Poppy Puppy');
-    console.log('Button has been clicked');
-    document.getElementById("demo").innerHTML = "Button has been clicked";
-}
+// function myFunction() {
+//     alert('Poppy Puppy');
+//     console.log('Button has been clicked');
+//     document.getElementById("text").innerHTML = "Button has been clicked";
+// }
+
+const test1 = 0.5;
+    // const test1 = 2.25;
+console.log(test1);
