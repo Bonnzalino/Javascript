2a795819312ef1c7e28d83a5b2b098ecce574e09
Bonnzalino <siwakorn.paorod@gmail.com>
2023-03-10 14:36:04 +0700

    Finished class

---
+++ 031023/script.js
@@ -0,0 +1,67 @@
+// console.log("Hello World!");
+
+// // let email = `Strom`;
+// // console.log(email);
+
+// //string concatenation
+// let firstName = `Bon`;
+// let lastName = `Kook kook`;
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// let info = `My name is ${fullName} and I am ${firstName}`;
+// console.log(info);
+
+// console.log(fullName.length);
+// console.log(fullName.toUpperCase());
+
+// let email = `siwakorn@gmail.com`;
+// let findLetter = email.includes(`k`);
+// console.log(findLetter);
+
+// let radius = 10;
+// const pi = 3.14;
+
+// console.log(radius, pi);
+
+// console.log(10/2);
+// let result = radius % 3;
+// console.log(result);
+
+// let bidMas = 5 * (10-3)**2;
+// console.log(bidMas);
+
+// let age;
+// console.log(age);
+
+// let selectedFruits = ['Apple', 'Banana', 'Orange'];
+// console.log(selectedFruits[1]);
+// selectedFruits [3] = 'Pineapple';
+// selectedFruits [4] = 9;
+// console.log(selectedFruits);
+// selectedFruits [1] = 'Pears';
+// console.log(selectedFruits);
+// console.log(selectedFruits.length);
+
+// let user = {
+//         firstName: 'Lak',
+//         LastName: 'Donovan',
+//         email: 'donovan@gmail.com',
+//         age: 26,
+//         location: 'UK',
+//         readingList:['HTML', 'CSS', 'JavaScript'],
+// }
+
+// console.log(user);
+// console.log(user.email);
+// user.age = 50;
+// console.log(user.age);
+// console.log(user.readingList[1]);
+
+//Converting Numbers to strings
+
+let x = 123;
+console.log(typeof x);
+x = "me";
+console.log(x);
+console.log(typeof x);
