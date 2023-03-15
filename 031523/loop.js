47257764104ac7a365ddf6dae6a9ff91212da4c7
Bonnzalino <siwakorn.paorod@gmail.com>
2023-03-15 15:06:34 +0700

    Add loop and array

---
+++ 031523/loop.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i < 5; i++){
+//     console.log('Hello World ' + i);
+// }
+
+// for (let i =0; i <=5; i++){
+//     if (i % 2 !== 0) console.log('I am odd ' + i);
+// }
+
+for (let i = 0; i < 5; i++){
+    console.log(i);
+    for (let j = 0; j < 3; j++)
+    console.log('Hello World ' + j);
+}
---
+++ 031523/script.js
@@ -0,0 +1,49 @@
+// const number = [3,4];
+// console.log(number);
+
+// //end
+// number.push(5,6);
+// console.log(number);
+
+// //begining
+// number.unshift(1,2);
+// console.log(number);
+
+// //middle
+// number.splice(1,0,'apple','7');
+// console.log(number);
+
+// const last = number.pop();
+// console.log(last);
+// console.log(number);
+
+// const middle = number.splice(1,2);
+// console.log(number);
+
+// const number = [1,2,3,1,4,5,1];
+
+// console.log(number.indexOf('1'));
+// console.log(number.indexOf(1));
+// console.log(number.lastIndexOf(1));
+
+// console.log(number.indexOf(1) !== -1);
+// console.log(number.includes(1));
+
+// const first = [1,2,3];
+// const second =[4,5,6];
+
+// const combined = first.concat(second);
+// console.log(combined);
+
+// const third = [...first,...second];
+// console.log(third);
+
+// const slice = combined.slice(2,4);
+// console.log(slice);
+
+const number = [2,3,1];
+number.sort();
+console.log(number);
+
+number.reverse();
+console.log(number);
